dcfed2755f0505bdef24e102acceaabeca33a687
Maksym Kaspriv <vestry.odessa@gmail.com>
2020-02-25 13:11:48 +0200

    feat: resolve array-typical-problems functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (array === undefined || array.length === 0) ? 0 : Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (array === undefined || array.length === 0) ? 0 : Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+
+  let result = array.reduce((sum, el) => sum + el);
+
+  return result / array.length;
 }
